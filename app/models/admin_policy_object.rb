b9f41c2ece9b38d6f7d648042f102b50de7c7a3f
John Scofield <john.scofield@yourmediashelf.com>
2011-02-22 19:11:06 -0500

    (Hydrus) HYDRUS: migrating HydraAdminPolicy to AdminPolicyObject
    Adjusts class/model names to match agreed-upon nomenclature
    for these objects: Admin Policy Objects (APOs).  The
    hydra_admin_policies plugin becomes admin_policy_objects plugin and
    this fix updates all specs  and features.

---
+++ app/models/admin_policy_object.rb
@@ -0,0 +1,25 @@
+require "lyber_core"
+
+
+class AdminPolicyObject <  Dor::Base
+ 
+   has_relationship "members", :is_member_of, :inbound=>true
+
+   # Both of these are Part of the dor_objects plugin. 
+   has_metadata :name => "identityMetadata", :type => Stanford::IdentityMetadataDs
+   has_metadata :name => "DC", :type => Stanford::SimpleDublinCoreDs
+ 
+   #these objects still need to have their OM models built. Still waiting on Lynn to explain what they are. 
+   has_metadata :name => "administrativeMetadata", :type => ActiveFedora::NokogiriDatastream     
+   has_metadata :name => "defaultObjectRights", :type => ActiveFedora::NokogiriDatastream
+  
+
+  # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
+  has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata 
+  
+  def to_solr(solr_doc=Solr::Document.new, opts={})
+    super
+    solr_doc << {:edit_access_group_t => "admin_policy_object_editor"}
+    solr_doc
+  end
+end
---
+++ lib/tasks/hydra_admin_set.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :admin_policy_object do
+#   # Task goes here
+# end
